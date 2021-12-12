9797a58d4aa9d00ebf0f10101722707450b10696
Abud-B <82672215+Abud-B@users.noreply.github.com>
2021-12-12 21:41:53 +0000

    understanding the code

--- src/App.vue
+++ src/App.vue
@@ -213,50 +213,31 @@
         this.$refs.calendar.next()
       },
       showEvent ({ nativeEvent, event }) {
-        const open = () => {
-          this.selectedEvent = event
-          this.selectedElement = nativeEvent.target
-          requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
-        }
-
         if (this.selectedOpen) {
           this.selectedOpen = false
-          requestAnimationFrame(() => requestAnimationFrame(() => open()))
         } else {
-          open()
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          this.selectedOpen = true
         }
 
         nativeEvent.stopPropagation()
       },
       updateRange ({ start, end }) {
-        const events = []
+        const events = [
+          {
+            name:"Coding lesson",
+            start: new Date('December 12, 2021 20:30:00'),
+            end: new Date('December 12, 2021 21:30:00'),
+            color: "deep-purple",
+            timed: true,
+          }
+        ]
 
-        const min = new Date(`${start.date}T00:00:00`)
-        const max = new Date(`${end.date}T23:59:59`)
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
-          events.push({
-            name: this.names[this.rnd(0, this.names.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            timed: !allDay,
-          })
-        }
-
+        console.log(start,end)
         this.events = events
       },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
+  
     },
   }
 </script>
