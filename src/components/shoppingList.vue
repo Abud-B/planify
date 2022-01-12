1608d8b039d8280a286c47da2e70de8c1d6a154b
Abud-B <82672215+Abud-B@users.noreply.github.com>
2022-01-12 15:50:31 +0000

    added unsplash api

--- src/components/shoppingList.vue
+++ src/components/shoppingList.vue
@@ -41,10 +41,15 @@ import Chips from "./Chips.vue"
     },
     methods:{
         fetchImage(itemName){
-            //fetch image
-            //return link
-        }
-    },
+           fetch(`https://api.unsplash.com/search/photos/?client_id=8Hw2GK9VED-X2W8Nb8DMcQ6RTCSC4T9onpEkG0x_G2U&query=${itemName}&content_filter=high`)
+            .then(data => data.json())
+            .then(data => {
+              let numResults = data.results.length
+              let randNum = Math.floor(Math.random() * numResults)
+              console.log( data.results[randNum].urls.small)
+            })
+          
+},
       props:{
           values:{
               type:Array
@@ -52,7 +57,7 @@ import Chips from "./Chips.vue"
         
   },
   components:{
-      Chips,
+      Chips
     }
-  }
+  }}
 </script>
